7649917c79ba9df9d8993080202cfe2962bae00b
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-08-02 07:32:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ load_dotenv()
 if __name__ == "__main__":
     app = Client(
         ":memory:",
-        api_id=os.environ.get("API_ID"),
-        api_hash=os.environ.get("API_HASH"),
-        bot_token=os.environ.get("BOT_TOKEN"),
+        api_id=os.environ.get("API_ID", 4665778),
+        api_hash=os.environ.get("API_HASH", "10e3ed833b0d09699973420d45359409"),
+        bot_token=os.environ.get("BOT_TOKEN", "5343045120:AAFx4-CSTguKQXWZFUG13mrjuRmk5pUdf60"),
         plugins=dict(root="modules"),
     )
     app.run()
